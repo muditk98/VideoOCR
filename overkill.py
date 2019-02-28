a23cc2e30a711ecd10312e66579c6bc0bd9d79e6
TheWussPuss <kapsmudit@gmail.com>
2019-02-28 18:05:56 +0530

    cleaned up code

--- overkill.py
+++ overkill.py
@@ -7,17 +7,25 @@ from matplotlib import pyplot as plt
 from pytesseract import Output
 
 
-def thressher(orig):
+def apply_thresholds(orig):
 	img = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
-
+	gauss_blurs = {
+		9: cv2.GaussianBlur(img, (9, 9), 0),
+		7: cv2.GaussianBlur(img, (7, 7), 0),
+		5: cv2.GaussianBlur(img, (5, 5), 0)
+	}
+	median_blurs = {
+		5: cv2.medianBlur(img, 5),
+		3: cv2.medianBlur(img, 3)
+	}
 	threshes = [
-		cv2.threshold(cv2.GaussianBlur(img, (9, 9), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
-		cv2.threshold(cv2.GaussianBlur(img, (7, 7), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
-		cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
-		cv2.threshold(cv2.medianBlur(img, 5), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
-		cv2.threshold(cv2.medianBlur(img, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
-		cv2.adaptiveThreshold(cv2.GaussianBlur(img, (5, 5), 0), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2),
-		cv2.adaptiveThreshold(cv2.medianBlur(img, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2),
+		cv2.threshold(gauss_blurs[9], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+		cv2.threshold(gauss_blurs[7], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+		cv2.threshold(gauss_blurs[5], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+		cv2.threshold(median_blurs[5], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+		cv2.threshold(median_blurs[3], 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+		cv2.adaptiveThreshold(gauss_blurs[5], 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2),
+		cv2.adaptiveThreshold(median_blurs[3], 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2),
 		# cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1],
 		# cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1],
 		# cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1],
@@ -33,7 +41,7 @@ def thressher(orig):
 		'gaus5+adagaus 31 2',
 		'gaus3+adagaus 31 2',
 	]
-	return threshes
+	return threshes, titles
 
 
 def tess(image):
@@ -52,38 +60,38 @@ def tess(image):
 
 
 def decode_predictions(scores, geometry):
-	(numRows, numCols) = scores.shape[2:4]
+	(num_rows, num_cols) = scores.shape[2:4]
 	rects = []
 	confidences = []
 
-	for y in range(numRows):
-		scoresData = scores[0, 0, y]
-		xData0 = geometry[0, 0, y]
-		xData1 = geometry[0, 1, y]
-		xData2 = geometry[0, 2, y]
-		xData3 = geometry[0, 3, y]
-		anglesData = geometry[0, 4, y]
+	for y in range(num_rows):
+		scores_data = scores[0, 0, y]
+		x_data0 = geometry[0, 0, y]
+		x_data1 = geometry[0, 1, y]
+		x_data2 = geometry[0, 2, y]
+		x_data3 = geometry[0, 3, y]
+		angles_data = geometry[0, 4, y]
 
-		for x in range(numCols):
-			if scoresData[x] < args["min_confidence"]:
+		for x in range(num_cols):
+			if scores_data[x] < args["min_confidence"]:
 				continue
 
 			(offsetX, offsetY) = (x * 4.0, y * 4.0)
 
-			angle = anglesData[x]
+			angle = angles_data[x]
 			cos = np.cos(angle)
 			sin = np.sin(angle)
 
-			h = xData0[x] + xData2[x]
-			w = xData1[x] + xData3[x]
+			h = x_data0[x] + x_data2[x]
+			w = x_data1[x] + x_data3[x]
 
-			endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
-			endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
-			startX = int(endX - w)
-			startY = int(endY - h)
+			end_x = int(offsetX + (cos * x_data1[x]) + (sin * x_data2[x]))
+			end_y = int(offsetY - (sin * x_data1[x]) + (cos * x_data2[x]))
+			start_x = int(end_x - w)
+			start_y = int(end_y - h)
 
-			rects.append((startX, startY, endX, endY))
-			confidences.append(scoresData[x])
+			rects.append((start_x, start_y, end_x, end_y))
+			confidences.append(scores_data[x])
 
 	return rects, confidences
 
@@ -91,70 +99,17 @@ def decode_predictions(scores, geometry):
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", type=str, help="path to input image")
 ap.add_argument("-east", "--east", type=str, help="path to input EAST text detector")
-ap.add_argument("-c", "--min-confidence", type=float, default=0.7,help="minimum probability required to inspect a region")
+ap.add_argument("-c", "--min-confidence", type=float, default=0.7, help="minimum confidence")
 ap.add_argument("-w", "--width", type=int, default=320, help="nearest multiple of 32 for resized width")
 ap.add_argument("-e", "--height", type=int, default=320, help="nearest multiple of 32 for resized height")
 ap.add_argument("-p", "--padding", type=float, default=0.0, help="amount of padding to add to each border of ROI")
 ap.add_argument("-l", "--lang", type=str, help="Language", default='eng')
 args = vars(ap.parse_args())
 
-image = cv2.imread(args["image"])
-orig = image.copy()
-(origH, origW) = image.shape[:2]
 
-(newW, newH) = (32 * (origW // 32), 32 * (origH // 32))
-
-rW = origW / float(newW)
-rH = origH / float(newH)
-
-image = cv2.resize(image, (newW, newH))
-(H, W) = image.shape[:2]
-
-layerNames = [
-	"feature_fusion/Conv_7/Sigmoid",
-	"feature_fusion/concat_3"
-]
-
-# print("[INFO] loading EAST text detector...")
-net = cv2.dnn.readNet(args["east"])
-
-blob = cv2.dnn.blobFromImage(image, 1.0, (W, H), (123.68, 116.78, 103.94), swapRB=True, crop=False)
-net.setInput(blob)
-(scores, geometry) = net.forward(layerNames)
-
-(rects, confidences) = decode_predictions(scores, geometry)
-boxes = non_max_suppression(np.array(rects), probs=confidences)
-
-results = []
-
-for (startX, startY, endX, endY) in boxes:
-	startX = int(startX * rW)
-	startY = int(startY * rH)
-	endX = int(endX * rW)
-	endY = int(endY * rH)
-
-	dX = int((endX - startX) * args["padding"])
-	dY = int((endY - startY) * args["padding"])
-
-	startX = max(0, startX - dX)
-	startY = max(0, startY - dY)
-	endX = min(origW, endX + (dX * 2))
-	endY = min(origH, endY + (dY * 2))
-
-	roi = orig[startY:endY, startX:endX]
-	th_imgs = thressher(roi)
-	for i in range(len(th_imgs)):
-		img = tess(th_imgs[i])
-		# plt.subplot(3,4,i+1)
-		plt.imshow(img)
-		plt.show()
-		# plt.title(titles[i])
-		# plt.xticks([]), plt.yticks([])
-		print('-' * 10)
-
-	# config = "--oem 1 --psm 7"
-	# text = pytesseract.image_to_string(roi, config=config, lang=args['lang'])
-	# results.append(((startX, startY, endX, endY), text))
+# config = "--oem 1 --psm 7"
+# text = pytesseract.image_to_string(roi, config=config, lang=args['lang'])
+# results.append(((startX, startY, endX, endY), text))
 
 # results = sorted(results, key=lambda r: r[0][1])
 #
@@ -172,3 +127,63 @@ for (startX, startY, endX, endY) in boxes:
 #
 # 	cv2.imshow("Text Detection", cv2.resize(output, (720, 480)))
 # 	cv2.waitKey(0)
+
+
+def __main__():
+	image = cv2.imread(args["image"])
+	orig = image.copy()
+	(origH, origW) = image.shape[:2]
+
+	(newW, newH) = (32 * (origW // 32), 32 * (origH // 32))
+
+	r_w = origW / float(newW)
+	r_h = origH / float(newH)
+
+	image = cv2.resize(image, (newW, newH))
+	(H, W) = image.shape[:2]
+
+	layer_names = [
+		"feature_fusion/Conv_7/Sigmoid",
+		"feature_fusion/concat_3"
+	]
+
+	# print("[INFO] loading EAST text detector...")
+	net = cv2.dnn.readNet(args["east"])
+
+	blob = cv2.dnn.blobFromImage(image, 1.0, (W, H), (123.68, 116.78, 103.94), swapRB=True, crop=False)
+	net.setInput(blob)
+	(scores, geometry) = net.forward(layer_names)
+
+	(rectangles, confidences) = decode_predictions(scores, geometry)
+	boxes = non_max_suppression(np.array(rectangles), probs=confidences)
+
+	# results = []
+
+	for (start_x, start_y, end_x, end_y) in boxes:
+		start_x = int(start_x * r_w)
+		start_y = int(start_y * r_h)
+		end_x = int(end_x * r_w)
+		end_y = int(end_y * r_h)
+
+		d_x = int((end_x - start_x) * args["padding"])
+		d_y = int((end_y - start_y) * args["padding"])
+
+		start_x = max(0, start_x - d_x)
+		start_y = max(0, start_y - d_y)
+		end_x = min(origW, end_x + (d_x * 2))
+		end_y = min(origH, end_y + (d_y * 2))
+
+		roi = orig[start_y:end_y, start_x:end_x]
+		threshold_images = apply_thresholds(roi)
+		for i in range(len(threshold_images)):
+			img = tess(threshold_images[i])
+			# plt.subplot(3,4,i+1)
+			plt.imshow(img)
+			plt.show()
+			# plt.title(titles[i])
+			# plt.xticks([]), plt.yticks([])
+			print('-' * 10)
+
+
+if __name__ == '__main__':
+	__main__()
